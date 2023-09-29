1098da01ecde0443fe6b84c7371164618a8ff939
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-09-29 13:51:57 -0700

    Reverted to older version then added third point to point class and generated methods.

--- src/main/java/making_changes/Point.java
+++ src/main/java/making_changes/Point.java
@@ -1,12 +1,22 @@
 package making_changes;
 
+import java.util.Objects;
+
+/**
+ * @author Katherine Watkins
+ * SDEV 372
+ * 9/29/2023
+ * Friday Lab #1
+ */
 public class Point {
     private double a;
     private double b;
+    private double c;
 
-    public Point(double a, double b) {
+    public Point(double a, double b, double c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
 
     public double getA() {
@@ -23,5 +33,39 @@ public class Point {
 
     public void setB(double b) {
         this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(getA(), point.getA()) == 0 && Double.compare(getB(), point.getB()) == 0 && Double.compare(getC(), point.getC()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getA(), getB(), getC());
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
     }
 }
